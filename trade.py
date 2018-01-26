ab95a47d82f69b38e134029852d208c8716a678a
John Singh <superjohn007@gmail.com>
2018-01-25 16:49:46 -0800

    Re-did the over enginered GetPercentage

--- trade.py
+++ trade.py
@@ -28,7 +28,7 @@ def Trailing(curpair, price, size, goal, originalPrice, stopLoss):
 		StoreOrder(str(stopLossSell["id"]), curpair, price, size, goal, originalPrice, stopLoss)
 		print("Order: " + str(stopLossSell["id"]))
 		print("Setting stop loss: " + str(stopLossSell["price"]))
-		print("Will start trailing at, " + str(price) + ", with TP at, " + str(GetPercentage(curpair, goal - 1, priceToUse=originalPrice)))
+		print("Will start trailing at, " + str(price) + ", with TP at, " + str(GetPercentage(goal - 1, originalPrice)))
 		time.sleep(5)
 		while (stopLossSell["status"] != "closed"):
 			currentPrice = float(ccxt.binance().fetch_ticker(curpair)['last'])
@@ -37,7 +37,7 @@ def Trailing(curpair, price, size, goal, originalPrice, stopLoss):
 			# If we've reached the point where we want to start a trail
 			if ("{:.7f}".format(float(currentPrice)) > "{:.7f}".format(float(price))):
 				# We're going to set our trailing price 1% lower than our take price (if we want to take at 5% we'll set our trail at 4%)
-				trailingPrice = GetPercentage(curpair, goal - 1, priceToUse=originalPrice)
+				trailingPrice = GetPercentage(goal - 1, originalPrice)
 				print("Setting take profit: " + str(trailingPrice))
 				DeleteOrder(str(stopLossSell["id"]))
 				binance.cancel_order(str(stopLossSell["id"]), curpair)
@@ -48,10 +48,10 @@ def Trailing(curpair, price, size, goal, originalPrice, stopLoss):
 				StoreOrder(str(profitSell["id"]), curpair, price, size, goal, originalPrice, stopLoss)
 				while(profitSell["status"] != "closed"):
 					print("Bought: " + str(originalPrice) + ", Current: " + str(currentPrice) + ", Stop: " + str(trailingPrice) + ", Increase: " + str(GetPercentage(curpair, goal + 1, priceToUse=originalPrice)) + ", Goal(%): " + str(goal))
-					if ("{:.7f}".format(float(currentPrice)) >= GetPercentage(curpair, goal + 1, priceToUse=originalPrice)):
+					if ("{:.7f}".format(float(currentPrice)) >= GetPercentage(goal + 1, originalPrice)):
 						goal += 1
 						binance.cancel_order(str(profitSell["id"]), curpair)
-						trailingPrice = GetPercentage(curpair, goal - 1, priceToUse=originalPrice)
+						trailingPrice = GetPercentage(goal - 1, originalPrice)
 						try:
 							profitSell = binance.createLimitSellOrder(curpair, size, trailingPrice, { 'type': 'stop_loss_limit', 'stopPrice': "{:7f}".format(float(trailingPrice)) })
 						except:
@@ -98,38 +98,18 @@ def RestartOrder(orderID):
 	DeleteOrder(orderID)
 	print(Trailing(curpair, price, size, goal, originalPrice, float(stopLoss)))
 
-# Here we're calculating the actual number values based on %'s we want to achieve
-# Need to rewrite, it's not a great implementation
-def GetPercentage(curpair, goal, stopLoss=False, priceToUse=0):
+def GetPercentage(goal, priceToUse, stopLoss=False):
 	try:
-		curpair = int(curpair)
-		if priceToUse == 0:
-			if stopLoss == True:
-				pairPrice = curpair
-				goalPrice = pairPrice - (pairPrice * (goal / 100))
-			else:
-				pairPrice = curpair
-				goalPrice = pairPrice + (pairPrice * (goal / 100))
-		elif priceToUse != 0:
-			if stopLoss == True:
-				goalPrice = priceToUse - (priceToUse * (goal / 100))
-			else:
-				goalPrice = priceToUse + (priceToUse * (goal / 100))
+		priceToUse = float(priceToUse)
+		# Are we calculating our profit, or our stop loss
+		if stopLoss == False:
+			goalPrice = priceToUse + (priceToUse * (goal / 100))
+		elif stopLoss == True:
+			goalPrice = priceToUse - (priceToUse * (goal / 100))
 		return float(goalPrice)
-	except:
-		if priceToUse == 0:
-			if stopLoss == True:
-				pairPrice = float(ccxt.binance().fetch_ticker(curpair)['last'])
-				goalPrice = pairPrice - (pairPrice * (goal / 100))
-			else:
-				pairPrice = float(ccxt.binance().fetch_ticker(curpair)['last'])
-				goalPrice = pairPrice + (pairPrice * (goal / 100))
-		elif priceToUse != 0:
-			if stopLoss == True:
-				goalPrice = priceToUse - (priceToUse * (goal / 100))
-			else:
-				goalPrice = priceToUse + (priceToUse * (goal / 100))
-		return "{:.7f}".format(float(goalPrice))
+	except Exception as e:
+		return str(e)
+
 
 def Menu():
 	print("\n\n(1) - Buy")
@@ -145,17 +125,17 @@ def Menu():
 		size = input("Size: ")
 		print(Buy(curpair, price, size))
 	if (selection == "2"):
-		curpair = input("Pair or Price: ")
+		originalPrice = float(input("Price: "))
 		goal = int(input("Goal (%): "))
-		print(GetPercentage(curpair, goal))
+		print(GetPercentage(goal, originalPrice))
 	if (selection == "3"):
 		curpair = input("Pair: ")
 		goal = int(input("Profit goal (%): "))
 		stopLoss = int(input("Stop loss (%): "))
-		price = float(GetPercentage(curpair, goal))
 		size = float(input("Size: "))
 		originalPrice = float(input("Entry Price: "))
-		stopLoss = GetPercentage(curpair, stopLoss, stopLoss=True)
+		price = float(GetPercentage(goal, originalPrice))
+		stopLoss = GetPercentage(stopLoss, originalPrice, stopLoss=True)
 		print(Trailing(curpair, price, size, goal, originalPrice, float(stopLoss)))
 	if (selection == "4"):
 		ListActiveOrders()
